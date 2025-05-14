78cb29fe8b12de4e27670f2c03113a532b2bf6cc
vijayrajarathinam <vijayrajarathinam93@gmail.com>
2025-05-14 18:02:53 +0530

    gh-pages: publish 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { AppProvider } from "@/context/AppContext";
 import NavigationBar from "@/components/NavigationBar";
 import HomePage from "@/pages/HomePage";
@@ -20,21 +19,30 @@ const queryClient = new QueryClient();
 
 const App = () => {
   // Check if user has completed onboarding
-  const onboardingCompleted = localStorage.getItem("onboarding_completed") === "true";
-  
+  const onboardingCompleted =
+    localStorage.getItem("onboarding_completed") === "true";
+
   return (
     <QueryClientProvider client={queryClient}>
       <AppProvider>
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <Router>
             <div className="max-w-lg mx-auto bg-white min-h-screen relative">
               <Routes>
-                <Route path="/" element={onboardingCompleted ? <HomePage /> : <OnboardingPage />} />
+                <Route
+                  path="/"
+                  element={
+                    onboardingCompleted ? <HomePage /> : <OnboardingPage />
+                  }
+                />
                 <Route path="/onboarding" element={<OnboardingPage />} />
                 <Route path="/appointments" element={<AppointmentsPage />} />
-                <Route path="/appointments/book" element={<BookAppointmentPage />} />
+                <Route
+                  path="/appointments/book"
+                  element={<BookAppointmentPage />}
+                />
                 <Route path="/timeline" element={<TimelinePage />} />
                 <Route path="/progress" element={<ProgressPage />} />
                 <Route path="/settings" element={<SettingsPage />} />
@@ -43,7 +51,7 @@ const App = () => {
               </Routes>
               {onboardingCompleted && <NavigationBar />}
             </div>
-          </BrowserRouter>
+          </Router>
         </TooltipProvider>
       </AppProvider>
     </QueryClientProvider>
