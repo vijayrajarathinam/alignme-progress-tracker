b2527cbe7f43f820d335449f524f508c5b1c252f
vijayrajarathinam <vijayrajarathinam93@gmail.com>
2025-05-14 18:06:06 +0530

    gh-pages: publish 3

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  // server: {
+  //   host: "::",
+  //   port: 8080,
+  // },
+  base: "/alignme-progress-tracker/",
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
