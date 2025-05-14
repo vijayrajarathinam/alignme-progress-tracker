62017fdadebf86c618516cf3527eef22e4d7ee19
vijayrajarathinam <vijayrajarathinam93@gmail.com>
2025-05-14 18:12:37 +0530

    gh-pages: publish 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  // server: {
-  //   host: "::",
-  //   port: 8080,
-  // },
+export default defineConfig(() => ({
   base: "/alignme-progress-tracker/",
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
